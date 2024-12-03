65d1ed4839a18fbc0e540eb0b3042c1ee270838e
NikosProtonotarios <147170230+NikosProtonotarios@users.noreply.github.com>
2024-12-03 05:14:41 +0000

    complete backend

--- Backend/Routers/user.Router.js
+++ Backend/Routers/user.Router.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const verifyToken = require("../middleware/auth");
 
 const {
-    registerUser,
-    loginUser,
     getFavoritesWithDetails,
     addFavoriteMovie,
     addToWatchlist,
     getWatchlist,
     deleteFavorite,
     deleteWatchedMovie,
+    login,
+    register,
 } = require('../controllers.js/user.controller');
 
-router.get('/user/favorites/:id', getFavoritesWithDetails);
-router.get('/user/watchlist/:id', getWatchlist);
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.post('/user/favorites', addFavoriteMovie);
-router.post('/user/watchlist', addToWatchlist);
-router.delete('/user/favorite', deleteFavorite);
-router.delete('/user/watchlist', deleteWatchedMovie);
+router.get('/user/favorites/:id', verifyToken, getFavoritesWithDetails);
+router.get('/user/watchlist/:id', verifyToken, getWatchlist);
+router.post('/user/favorites', verifyToken, addFavoriteMovie);
+router.post('/user/watchlist', verifyToken, addToWatchlist);
+router.delete('/user/favorite', verifyToken, deleteFavorite);
+router.delete('/user/watchlist', verifyToken, deleteWatchedMovie);
+router.post('/login', login);
+router.post('/register', register);
 
 module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,7 @@ const userRoutes = require('./Routers/user.Router');
 const movieRoutes = require('./Routers/movies.Router');
 const commentsRoutes = require('./Routers/comments.Router');
 const User = require('./models/userModel');
+const verifyToken = require('./middleware/auth');
 
 // initialize the app
 const app = express();
@@ -16,70 +17,6 @@ const port = 8080;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-// Register a new user
-app.post('/register', async(req, res) => {
-    // let email = req.body.email;
-    // let password = req.body.password;
-    // let username = req.body.username;
-    try {
-        let {email, password, username} = req.body;
-        if (!email || !password || !username) {
-            return res.status(400).json({ message: 'Please fill in all fields' });
-        }
-        let oldUser = await User.findOne({ email });
-        if (oldUser) {
-            return res.status(400).json({ message: 'Email already exists' });
-        }
-    
-        let hashedPassword = await bcrypt.hash(password, 10);
-    
-        await User.create({
-            email,
-            password: hashedPassword,
-            username,
-        });
-        
-        return res.send({message: "Registered succesfully"});
-    } catch (error) {
-        return res.status(500).send({msg: "Internal server error"});
-    }
-});
-
-app.post('/login', async (req, res) => {
-    try {
-        let {email, password} = req.body;
-    if(!email || !password) {
-        return res.send({
-            msg: "Please fill in all fields",
-        });
-    }
-    let registeredUser = await User.findOne({email});
-    if(!registeredUser) {
-        return res.send({msg: "User not found, please register first"});
-    }
-    let isPasswordCorrect = await bcrypt.compare(
-        password,
-        registeredUser.password
-    );
-    if(!isPasswordCorrect) {
-        return res.send({msg: "incorrect password"});
-    }
-
-    // Return Token (payload, secretkey)
-
-    // Generate the Token
-    let payload = {
-        userId: registeredUser._id,
-        email: registeredUser.email,
-    }
-    let token = jwt.sign(payload, "supersecretawesomness");
-
-    return res.send({msg: "Logged in successfully", token});
-    } catch (error) {
-        return res.status(500).send({msg: "Internal server error"});
-    }
-});
 
 app.use('/', userRoutes);
 app.use('/movies', movieRoutes);
